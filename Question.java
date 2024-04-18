fcb144f47d7ce1e880de9b685e9263f62f33829d
D070508T <davidt070508@gmail.com>
2024-04-18 14:14:01 -0400

    Commit

--- Question.java
+++ Question.java
@@ -6,16 +6,52 @@ public class Question {
     private ArrayList<Boolean> correct;
     private Quiz parentQuiz;
 
+    public Question() {
+        question = "";
+        answers = new ArrayList<String>();
+        correct = new ArrayList<Boolean>();
+        parentQuiz = new Quiz();
+    }
+
+    public Question(Quiz quiz) {
+        question = "";
+        answers = new ArrayList<String>();
+        correct = new ArrayList<Boolean>();
+        parentQuiz = quiz;
+    }
+
     public Question(String _question, Quiz parent) {
         question = _question;
         answers = new ArrayList<String>();
         correct = new ArrayList<Boolean>();
+        parentQuiz = parent;
     }
 
-    public Question(String question, ArrayList<String> answers, ArrayList<Boolean> correct, Quiz parentQuiz) {
+    public Question(String question, ArrayList<String> answers, ArrayList<Boolean> correct, Quiz parent) {
         this.question = question;
         this.answers = answers;
         this.correct = correct;
+        parentQuiz = parent;
+    }
+
+    public void setQuestion(String _question) {
+        question = _question;
+    }
+
+    public void changeAnswerValue(String answer, boolean value) {
+        for (int i = 0; i < answers.size(); i++) {
+            if (answers.get(i).equals(answer)) {
+                correct.set(i, value);
+            }
+        }
+    }
+
+    public void changeAnswer(String answer, String newAnswer) {
+        for (String answerIndex : answers) {
+            if (answerIndex.equals(answer)) {
+                answerIndex = newAnswer;
+            }
+        }
     }
 
     public String getQuestion() {
